dc4077d891a7b9c6fd3210b1f556fbe89f76229e
ErfanAalam <erfankhansiwani@gmail.com>
2024-09-12 00:05:11 +0530

    Files added

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def hello():
+    data = None
+    if request.method == "POST":
+        city = request.form.get('city')
+        respose = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city},india&units=imperial&appid=d2a02760d2ef58d25d2c6e3f0fb2c70e")
+    
+        data = respose.json()
+    
+    return render_template("weather.html", data=data)
+
+
+if(__name__=="__main__"):
+    app.run(debug=True)
